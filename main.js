c8afe39dd4be42e485d59f1f2a1bd26acd1ff7d5
Olga Albukova <o.albukova@gmail.com>
2020-09-24 20:59:32 +0300

    with second

--- main.js
+++ main.js
@@ -26,3 +26,29 @@ function getRow(firstRow, secondRow) {
 }
 
 alert(getRow(firstRow, secondRow));
+
+function formattedPhone(phone) {
+  let result = "";
+  result =
+    phone.charAt(0) +
+    phone.charAt(1) +
+    " " +
+    "(" +
+    phone.charAt(2) +
+    phone.charAt(3) +
+    phone.charAt(4) +
+    ")" +
+    " " +
+    phone.charAt(5) +
+    phone.charAt(6) +
+    phone.charAt(7) +
+    "-" +
+    phone.charAt(8) +
+    phone.charAt(9) +
+    "-" +
+    phone.charAt(10) +
+    phone.charAt(11);
+  return result;
+}
+
+alert(formattedPhone("+71234567890"));
