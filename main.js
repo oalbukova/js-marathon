15fb182b60996dd133cc269528c32f6dd3d5277a
Olga Albukova <o.albukova@gmail.com>
2020-09-25 02:22:37 +0300

    finish two

--- main.js
+++ main.js
@@ -2,53 +2,73 @@ const firstRow = "мама мыла раму";
 const secondRow = "собака друг человека";
 const letter = prompt("Введите букву");
 
-function getRow(firstRow, secondRow) {
-  let count1 = 0;
-  let count2 = 0;
-
-  for (let i = 0; i <= firstRow.length; i++) {
-    if (firstRow.charAt(i) === letter) {
-      count1++;
+function iteratorFunc(nameRow) {
+  let count = 0;
+  for (let i = 0; i <= nameRow.length; i++) {
+    if (nameRow.charAt(i) === letter) {
+      count++;
     }
   }
-
-  for (let i = 0; i <= secondRow.length; i++) {
-    if (secondRow.charAt(i) === letter) {
-      count2++;
-    }
-  }
-
-  if (count1 === count2) {
-    return "количество букв одинаково в обоих фразах";
-  } else {
-    return count1 > count2 ? firstRow : secondRow;
-  }
+  return count;
 }
 
+function getRow(firstRow, secondRow) {
+  if (iteratorFunc(firstRow) === 0 && iteratorFunc(secondRow) === 0) {
+    return "этой буквы нет в данных фразах";
+  } else if (iteratorFunc(firstRow) === iteratorFunc(secondRow)) {
+    return "количество букв одинаково в обоих фразах";
+  } else {
+    return iteratorFunc(firstRow) > iteratorFunc(secondRow)
+      ? firstRow
+      : secondRow;
+  }
+}
 alert(getRow(firstRow, secondRow));
+
+
+const phone = prompt("Введите номер телефона");
 
 function formattedPhone(phone) {
   let result = "";
-  result =
-    phone.charAt(0) +
-    phone.charAt(1) +
-    " " +
-    "(" +
-    phone.charAt(2) +
-    phone.charAt(3) +
-    phone.charAt(4) +
-    ")" +
-    " " +
-    phone.charAt(5) +
-    phone.charAt(6) +
-    phone.charAt(7) +
-    "-" +
-    phone.charAt(8) +
-    phone.charAt(9) +
-    "-" +
-    phone.charAt(10) +
-    phone.charAt(11);
+
+  for (let i = 0; i <= phone.length; i++) {
+    if (phone.length === 12) {
+      if (i === 1) {
+        result += phone.charAt(i) + " " + "(";
+      } else if (i === 5) {
+        result += ")" + " " + phone.charAt(i);
+      } else if (i === 8) {
+        result += "-" + phone.charAt(i);
+      } else if (i === 10) {
+        result += "-" + phone.charAt(i);
+      } else result += phone.charAt(i);
+    }
+    else if (phone.length === 11) {
+            if (i === 0) {
+              result += '+' + "7" + ' ' + "(";
+            } else if (i === 4) {
+              result += ")" + " " + phone.charAt(i);
+            } else if (i === 7) {
+              result += "-" + phone.charAt(i);
+            } else if (i === 9) {
+              result += "-" + phone.charAt(i);
+            } else result += phone.charAt(i);
+    }
+      else if (phone.length === 10) {
+        if (i === 0) {
+          result += "+" + "7" + " " + "(" + phone.charAt(i);
+        } else if (i === 3) {
+          result += ")" + " " + phone.charAt(i);
+        } else if (i === 6) {
+          result += "-" + phone.charAt(i);
+        } else if (i === 8) {
+          result += "-" + phone.charAt(i);
+        } else result += phone.charAt(i);
+    }
+    else return "введите номер телефона в верном формате";
+  }
   return result;
 }
 
-alert(formattedPhone("+71234567890"));
+alert(formattedPhone(phone)); 
+
