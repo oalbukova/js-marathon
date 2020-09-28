d18f11ff966463c647bf1be5e2102d5ddd397b7e
Olga Albukova <o.albukova@gmail.com>
2020-09-28 17:46:53 +0300

    fix

--- main.js
+++ main.js
@@ -19,9 +19,6 @@ const enemy = {
   elProgressbar: document.getElementById('progressbar-enemy')
 }
 
-<<<<<<< HEAD
-const phone = prompt("Введите номер телефона");
-=======
 function init() {
   console.log('Start Game!');
   renderHP(character);
@@ -32,7 +29,6 @@ function renderHP(person) {
   renderHPLife(person);
   renderProgressbarHP(person);
 }
->>>>>>> 67a57b2c44b84ec3eb57bede8f4d0d8fccdd5b79
 
 function renderHPLife(person) {
   person.elHP.innerText = person.damageHP + '/' + person.defaultHP;
@@ -42,40 +38,6 @@ function renderProgressbarHP(person) {
   person.elProgressbar.style.width = person.damageHP + '%';
 }
 
-<<<<<<< HEAD
-  for (let i = 0; i <= phone.length; i++) {
-    if (phone.length === 12) {
-      if (i === 1) {
-        result += phone.charAt(i) + " " + "(";
-      } else if (i === 5) {
-        result += ")" + " " + phone.charAt(i);
-      } else if (i === 8) {
-        result += "-" + phone.charAt(i);
-      } else if (i === 10) {
-        result += "-" + phone.charAt(i);
-      } else result += phone.charAt(i);
-    } else if (phone.length === 11) {
-      if (i === 0) {
-        result += "+" + "7" + " " + "(";
-      } else if (i === 4) {
-        result += ")" + " " + phone.charAt(i);
-      } else if (i === 7) {
-        result += "-" + phone.charAt(i);
-      } else if (i === 9) {
-        result += "-" + phone.charAt(i);
-      } else result += phone.charAt(i);
-    } else if (phone.length === 10) {
-      if (i === 0) {
-        result += "+" + "7" + " " + "(" + phone.charAt(i);
-      } else if (i === 3) {
-        result += ")" + " " + phone.charAt(i);
-      } else if (i === 6) {
-        result += "-" + phone.charAt(i);
-      } else if (i === 8) {
-        result += "-" + phone.charAt(i);
-      } else result += phone.charAt(i);
-    } else return "введите номер телефона в верном формате";
-=======
 function buttonDisabled() {
   $btnKickJolt.disabled = true;
   $btnKickBolt.disabled = true;
@@ -89,7 +51,6 @@ function changeHP(count, person) {
     buttonDisabled();
   } else {
     person.damageHP -= count;
->>>>>>> 67a57b2c44b84ec3eb57bede8f4d0d8fccdd5b79
   }
   renderHP(person);
 }
@@ -98,9 +59,6 @@ function random(num) {
   return Math.ceil(Math.random() * num)
 }
 
-<<<<<<< HEAD
-alert(formattedPhone(phone));
-=======
 function setEventListener(num) {
   console.log('Kick');
   changeHP(random(num), character);
@@ -113,4 +71,3 @@ $btnKickJolt.addEventListener('click', () => setEventListener(15));
 $btnKickBolt.addEventListener('click', () => setEventListener(25));
 
 
->>>>>>> 67a57b2c44b84ec3eb57bede8f4d0d8fccdd5b79
